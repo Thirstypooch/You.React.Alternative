8338b5715528f1c01828df9cf7d9ccb74c175f23
Thirstypooch <thirstypooch@protonmail.com>
2020-12-25 21:15:03 -0500

    display ultimos productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Slide from './components/Slide'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
         <main className='py-3' style={{ background: '#FCF3E8' }}>
           <Container style={{ background: '#FCF3E8' }}>
             <Slide />
+            <HomeScreen/>
           </Container>
         </main>
       <Footer />
