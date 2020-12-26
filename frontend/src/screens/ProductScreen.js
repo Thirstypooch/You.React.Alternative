aead6f2b2201f5c7d48eaa853aafd002b6611eb0
Thirstypooch <thirstypooch@protonmail.com>
2020-12-26 00:11:58 -0500

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id)
   return (
-    <div>
-      Product
-    </div>
+    <>
+    <Link className='btn btn-light my-3' to='/'>
+      Regresa
+    </Link>
+    <Row>
+      <Col md={6}>
+        <Image src={product.image} alt={product.name} fluid/>
+      </Col>
+      <Col md={3}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h2>{product.name}</h2>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            ${product.price}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {product.description}
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col md={3}>
+        <Card>
+          
+        </Card>
+      </Col>
+    </Row>
+  </>
   )
 }
 
