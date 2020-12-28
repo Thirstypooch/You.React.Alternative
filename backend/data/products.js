8b363cbce85492ca6c7c41397a1b052c32847a76
Thirstypooch <thirstypooch@protonmail.com>
2020-12-28 17:21:52 -0500

    moving to es modules

--- backend/data/products.js
+++ backend/data/products.js
@@ -313,4 +313,4 @@ const products = [
   },
 ]
 
-module.exports = products
+export default products
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("API is running 🦿");
-});
+app.get('/', (req, res) => {
+  res.send('API is running 🦿')
+})
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json(products)
-}); 
+})
 
-app.get("/api/products/:id", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
-  res.json(product);
-}); 
+  res.json(product)
+})
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`💻 running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`💻 running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
