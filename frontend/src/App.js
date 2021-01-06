b14f4686bb90899d5caf261305b2e560a6d31b07
Thirstypooch <thirstypooch@protonmail.com>
2021-01-05 23:34:55 -0500

    qty select & cart route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,22 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <Router>
       <Header />
-        <main className='py-3' style={{ background: '#FCF3E8' }}>
-          <Container style={{ background: '#FCF3E8' }}>
-            
-            <Route path='/' component={HomeScreen} exact/>
-            <Route path='/product/:id' component={ProductScreen} />
-          </Container>
-        </main>
+      <main className='py-3' style={{ background: '#FCF3E8' }}>
+        <Container style={{ background: '#FCF3E8' }}>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+        </Container>
+      </main>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
