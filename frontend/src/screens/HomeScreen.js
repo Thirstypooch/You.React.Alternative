a21e2ff1769c50246c605dcdf3eaa100ed3af961
Thirstypooch <thirstypooch@protonmail.com>
2021-01-05 21:40:55 -0500

    bringing redux state into home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,68 +1,68 @@
-import React, { useState, useEffect } from 'react'
-//import Slide from './components/Slide'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 
-import axios from 'axios'
 import Carousel from 'react-bootstrap/Carousel'
 import img1 from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import img3 from '../img/img3.jpg'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async() => {
-      const { data } = await axios.get('/api/products')
+    dispatch(listProducts())
+  }, [dispatch])
 
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
   return (
     <>
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={img1}
-          alt='First slide'
-        />
-        <Carousel.Caption>
-          <h3>Creemos en el poder de lo natural </h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={img2}
-          alt='Third slide'
-        />
+      <Carousel>
+        <Carousel.Item>
+          <img className='d-block w-100' src={img1} alt='First slide' />
+          <Carousel.Caption>
+            <h3>Creemos en el poder de lo natural </h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src={img2} alt='Third slide' />
 
-        <Carousel.Caption>
-          <p>Creamos  productos naturales para el cuidado de la piel  efectivos de gran calidad ,orgánicos, de alta concentración y no tóxicos.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src={img3}
-          alt='Third slide'
-          width={384} height={575.833} 
-        />
-      </Carousel.Item>
-    </Carousel>
-  
+          <Carousel.Caption>
+            <p>
+              Creamos productos naturales para el cuidado de la piel efectivos
+              de gran calidad ,orgánicos, de alta concentración y no tóxicos.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={img3}
+            alt='Third slide'
+            width={384}
+            height={575.833}
+          />
+        </Carousel.Item>
+      </Carousel>
+
       <h1 className='text-center py-3'>Últimos productos</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Cargando...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
