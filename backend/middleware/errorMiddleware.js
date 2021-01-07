4843af58a034f29bab09c0f08b05f7269d76984b
Thirstypooch <thirstypooch@protonmail.com>
2021-01-06 19:04:55 -0500

    user auth

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,11 +1,11 @@
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
-  res.status(4004)
+  res.status(404)
   next(error)
 }
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : restart.statusCode
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
   res.status(statusCode)
   res.json({
     message: err.message,
