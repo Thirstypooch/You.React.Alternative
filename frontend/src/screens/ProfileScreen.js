32ed3d368ce8476a570fcb4715a8e3edd1637412
Thirstypooch <thirstypooch@protonmail.com>
2021-01-11 18:21:03 -0500

    profile screen

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Row, Col, Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { getUserDetails } from '../actions/userActions.js'
+
+const ProfileScreen = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    } else {
+      if (!user.name) {
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, history, userInfo, user])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('Las contraseñas no son iguales')
+    } else {
+      // dispatch update profile
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>Perfil de usuaria</h2>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name'>
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type='name'
+              placeholder='Ingrese nombre'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Ingrese email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='password'>
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Ingrese contraseña'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirme contraseña</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirme contraseña'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type='submit' variant='primary'>
+            Actualize
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>Mis ordenes</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
