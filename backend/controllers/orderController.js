6d11959b35cd9587b5bc9222efeea8db06f702e1
Thirstypooch <thirstypooch@protonmail.com>
2021-01-20 12:54:38 -0500

    order controller & route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+
+// @desc  Create new order
+// @route POST /api/orders
+// @access Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body
+
+  if(orderItems && orderItems === 0) {
+    res.status(400)
+    throw new Error('No hay items para ordenar')
+    return
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+    })
+
+    const createOrder = await order.save()
+
+    res.status(201).json(createOrder)
+  }
+})
+
+export { addOrderItems }
