b743eb673c601b3a5a102452fd73e2f26164f4cd
Thirstypooch <thirstypooch@protonmail.com>
2021-02-01 22:33:13 -0500

    get user by id and update user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,6 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-
 // @desc  Register a new user
 // @route POST /api/users
 // @access Public
@@ -33,18 +32,18 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email })
 
-  if(userExists) {
+  if (userExists) {
     res.status(400)
     throw new Error('El usuario ya existe')
   }
-  
+
   const user = await User.create({
-    name, 
+    name,
     email,
-    password
+    password,
   })
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -52,12 +51,11 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     })
-  } else  {
+  } else {
     res.status(400)
     throw new Error('La data del usuario es inválida')
   }
 })
-
 
 // @desc  GET user profile
 // @route GET /api/users/profile
@@ -65,7 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
-  if(user) {
+  if (user) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -84,13 +82,13 @@ const getUserProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
-  if(user) {
+  if (user) {
     user.name = req.body.name || user.name
     user.email = req.body.email || user.email
-    if(req.body.password) {
+    if (req.body.password) {
       user.password = req.body.password
     }
-    
+
     const updatedUser = await user.save()
     res.json({
       _id: updatedUser._id,
@@ -111,7 +109,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({})
   res.json(users)
-
 })
 
 // @desc  Delete user
@@ -119,15 +116,60 @@ const getUsers = asyncHandler(async (req, res) => {
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id)
-  
-  if(user) {
+
+  if (user) {
     await user.remove()
-    res.json({ message: 'Usuario removido'})
+    res.json({ message: 'Usuario removido' })
   } else {
     res.status(404)
     throw new Error('User not found')
   }
-
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
+// @desc  Get user by ID
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc  Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+}
